11d8245948d88ae1a3f96f64c049d3f45fce96d0
patrikstrausz <patrik.strausz@akademiasovy.sk>
2019-10-17 12:47:23 +0200

    Update

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package sk.itsovy.strausz.projectfragment;
 import  sk.itsovy.strausz.projectfragment.Fragment;
-
+import  sk.itsovy.strausz.projectfragment.MixedNumber;
 
 
 public class Main {
@@ -27,6 +27,30 @@ public class Main {
         Fragment f4 = f2.copy();
 
         Fragment f5 = new Fragment(fragment);
+
+
+        MixedNumber m1 = new MixedNumber(4,2,7);
+        System.out.println(m1.toString());
+
+        System.out.println(m1.convertToFragment());
+
+        System.out.println(m1.getRealValue());
+
+
+        Fragment f6 = new Fragment(9,3);
+        Fragment f7 = new Fragment(3,5);
+        MyMath math = new MyMath();
+
+
+        Fragment f8 = new Fragment(12, 5);
+        Fragment f9 = new Fragment(10,18);
+
+        System.out.println(math.mult(f8,f9));
+
+        System.out.println(math.div(f8,f9));
+
+        f6 = math.subb(f8,f9);
+        System.out.println( f6.toString());
 
     }
 
